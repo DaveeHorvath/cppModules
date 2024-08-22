258b9e8893a8c2f1a6c6e80ce4c3139b356ef01d
dhorvath <dhorvath@student.hive.fi>
2024-08-22 16:02:54 +0300

    added exception

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -5,11 +5,14 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
-
+#include <exception>
 
 template<typename T>
 typename T::iterator easyfind(T container, int val) { 
-    return std::find(container.begin(), container.end(), val);
+    typename T::iterator it = std::find(container.begin(), container.end(), val);
+    if (it == container.end())
+        throw std::runtime_error("Value isnt in container");
+    return it;
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,4 +3,11 @@
 int main(){
     std::vector<int> v = {1,2,3,4,5,6,7,8};
     std::cout << *easyfind<std::vector<int>>(v, 1) << "\n";
+    try {
+        std::cout << *easyfind<std::vector<int>>(v, 9) << "\n";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
 }
